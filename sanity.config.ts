00d5206fa2a9782714c56a01b8ea75cb02d1db38
TimourBussy <timourjbanov@gmail.com>
2026-03-29 15:35:25 +0200

    Remove .env

--- sanity.config.ts
+++ sanity.config.ts
@@ -2,16 +2,13 @@ import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemaTypes'
-import dotenv from 'dotenv'
-
-dotenv.config({path: '.env.local'})
 
 export default defineConfig({
   name: 'default',
   title: 'domiemorine',
 
-  projectId: process.env.VITE_SANITY_PROJECT_ID || '',
-  dataset: process.env.VITE_SANITY_DATASET || '',
+  projectId: 'sedrvse1',
+  dataset: 'production',
 
   plugins: [structureTool(), visionTool()],
 
